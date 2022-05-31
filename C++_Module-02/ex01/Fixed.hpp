ef85939eba4a7a2d93764efe4b537feffa4bdc49
DayneeBoiiz <saad.ayar@hotmail.com>
2022-05-31 14:50:57 +0100

    C++

--- C++_Module-02/ex01/Fixed.hpp
+++ C++_Module-02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: sayar <sayar <sayar@student.42.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/28 22:28:49 by sayar             #+#    #+#             */
-/*   Updated: 2022/05/28 22:29:15 by sayar            ###   ########.fr       */
+/*   Updated: 2022/05/31 13:24:37 by sayar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
+class	Fixed
+{
+private:
+	int					integer;
+	static const int	s_integer;
+public:
+	Fixed(void);
+	Fixed(const Fixed& _Fixed);
+	Fixed(const int integer);
+	Fixed(const float integer);
+	Fixed & operator = (const Fixed& _Fixed);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
+};
 
+std::ostream &operator << (std::ostream &COUT, Fixed const &_Fixed);
 
 #endif
